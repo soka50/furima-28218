befef214db4b93b015ad9e6d5e91cd93c291d41b
soka50 <2874linh@jcom.zaq.ne.jp>
2021-07-29 20:31:43 +0900

    商品詳細表示機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-   
+
   def show
     @item = Item.find(params[:id])
   end
