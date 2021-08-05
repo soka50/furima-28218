362be4be4a51debdf9c8098b36fbcb9822ac3a5c
soka50 <2874linh@jcom.zaq.ne.jp>
2021-08-05 20:33:20 +0900

    エラー解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :authenticate_user!, except: [:index, :show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -33,8 +33,13 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    Item.update(item_params)
+    
+    if @item.update(item_params)
+      @item.save
     redirect_to item_path
+   else
+    render :edit
+  end
   end
 
   private
