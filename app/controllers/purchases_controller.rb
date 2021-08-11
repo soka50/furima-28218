3e44b17ed132de7481eeefff54ac8e5775ce7f87
soka50 <2874linh@jcom.zaq.ne.jp>
2021-08-11 21:52:14 +0900

    エラーが表示

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,24 @@
+class PurchasesController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id )
+  end
+
+end
