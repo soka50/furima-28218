594a6858d064ab502866f72c647bb8cf027075bf
soka50 <2874linh@jcom.zaq.ne.jp>
2021-08-13 15:49:15 +0900

    テストコードを実装

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order = FactoryBot.build(:order, user_id: user.id, item_id: item.id)
+    sleep(0.5)
+  end
+
+
+  describe '#create' do
+    context '内容に問題がない場合' do
+      it 'building_nameは空でも保存できること' do
+        @order.building_name = ''
+        expect(@order).to be_valid
+    end
+    end
+
+    context '内容に問題が有る場合' do
+
+
+      it '郵便番号が必須であること' do
+        @order.postal_code = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it '都道府県が必須であること' do
+        @order.prefecture_id = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '市区町村が必須であること' do
+        @order.city = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("City can't be blank")
+      end
+
+      it '番地が必須であること' do
+        @order.house_number = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("House number can't be blank")
+      end
+
+      it '電話番号が必須であること' do
+        @order.phone_number= ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it '郵便番号にはハイフンが必要であること（123-4567となる）' do
+        @order.postal_code = '1234567'
+        @order.valid?
+        expect(@order.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+
+      it '電話番号にはハイフンは不要で、11桁以内であること' do
+        @order.phone_number = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      
+
+
+      
+      
+      
+    end
+  end
+end
