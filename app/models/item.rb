87aa1dd6edbfaa145ad801f61a4a04459512a1c4
soka50 <2874linh@jcom.zaq.ne.jp>
2021-08-15 19:02:39 +0900

    商品購入機能　再々修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  has_one :purchaser
+  has_one :purchase
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :product_condition
