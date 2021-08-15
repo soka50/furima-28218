1a77da3486933ed4d6c48497f09164ee74543d39
soka50 <2874linh@jcom.zaq.ne.jp>
2021-08-15 21:29:01 +0900

    Revert "商品購入機能　再々修正"
    This reverts commit 87aa1dd6edbfaa145ad801f61a4a04459512a1c4.

--- README.md
+++ README.md
@@ -62,7 +62,7 @@
 | post_code      | string     | null: false |
 | building_name  | string     |             |
 | phone_number   | string     | null: false |
-| purchase      | references | null: false, foreign_key: true |
+| purchaser      | references | null: false, foreign_key: true |
 ### Association
 
 - belongs_to :purchase 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'orders/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :items do
-    resources :purchases, only: [:create, :index]
-  end
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
